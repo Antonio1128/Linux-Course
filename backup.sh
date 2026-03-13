bb01937659150b81f949e4a5883511ecdf7a8059
Antonio1128 <antonio.popelca2811@gmail.com>
2026-03-13 21:28:24 +0200

    added system info and backup scripts

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting backup...."
+mkdir -p backup
+cp -r /mnt/c/Users/anton/Documents/ backup/
+echo "Backup finished!"
+
---
+++ system_info.sh
@@ -0,0 +1,17 @@
+echo  " === SYSTEM INFO ==="
+echo "user: $(whoami)"
+echo "date: $(date)"
+echo "current dirctory: $(pwd)"
+
+echo" "
+echo "=== System ==="
+unname  -a
+
+echo" "
+echo "=== Disk Usage ==="
+df -h
+
+echo ""
+echo "=== Memory ==="
+free  -h
+
